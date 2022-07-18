a0d3b4973f5eb0c94a67e9156e63ca1b4c3968f7
Joao Alberto <joaoalbertosv15@gmail.com>
2022-07-18 12:38:13 -0300

    Feat: Terminando a tela de login

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Button as ButtonNativeBase,
+  IButtonProps,
+  Heading
+} from 'native-base';
+
+type Props =  IButtonProps & {
+  title: string;
+}
+
+export default function Button({title, ...rest}: Props){
+  return (
+    <ButtonNativeBase 
+      {...rest}
+      bg="green.700"
+      h={14}
+      fontSize="sm"
+      rounded="sm"
+      _pressed={{
+        bg: 'green.500'
+      }}
+    >
+      <Heading
+        color="white"
+        fontSize="sm"
+      >
+        {title}
+      </Heading>
+    </ButtonNativeBase>
+  );
+}
