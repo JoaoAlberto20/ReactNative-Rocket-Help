9fe920b456f3822cc8b83a6defc79281a3879f4f
Joao Alberto <joaoalbertosv15@gmail.com>
2022-07-19 12:04:14 -0300

    Feat: Adicionando a biblioteca react Navigation

--- App.tsx
+++ App.tsx
@@ -8,7 +8,8 @@ import { THEME } from './src/styles/theme'
 import Loading from './src/components/Loading';
 // import SignIn from './src/screens/SignIn';
 // import Home from './src/screens/Home';
-import Register from './src/screens/Register';
+// import Register from './src/screens/Register';
+import Details from './src/screens/Details';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -20,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Register /> : <Loading />}
+      {fontsLoaded ? <Details /> : <Loading />}
     </NativeBaseProvider>
   );
 }
